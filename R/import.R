fb0dd094b25c6315341d3be4213c7123703ae0b6
Charlotte Wickham <cwickham@gmail.com>
2024-04-11 10:51:55 -0700

    Reorg

--- R/import.R
+++ R/import.R
@@ -2,10 +2,11 @@ library(googledrive)
 library(googlesheets4)
 library(yaml)
 library(dplyr)
+library(here)
 
 apps <- drive_get(id = as_id("12HDbqfXia6gq4koKHf6DGgOgWGkq_hl8o6iJxIG4Ucw")) |> 
   read_sheet() 
 
 apps |> 
   select(url = Application, org = Organization, title = Project) |> 
-  write_yaml("apps.yml", column.major = FALSE)
+  write_yaml(here("apps.yml"), column.major = FALSE)
