9cbe21118561fea8fc0641d3ae9f224986427dce
Charlotte Wickham <cwickham@gmail.com>
2024-05-09 10:09:43 -0700

    Update app list

--- R/import.R
+++ R/import.R
@@ -5,10 +5,10 @@ library(readr)
 library(here)
 
 sheet <- drive_download(as_id("12HDbqfXia6gq4koKHf6DGgOgWGkq_hl8o6iJxIG4Ucw"),
-  here("data", "raw.csv"))
+  here("data", "raw.csv"), overwrite = TRUE)
 
 apps <- read_csv(here("data", "raw.csv"))
 
 apps |> 
-  select(url, org, title, thumbnail) |> 
+  select(url, org, title, thumbnail, description) |> 
   write_yaml(here("apps.yml"), column.major = FALSE)
